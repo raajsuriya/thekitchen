66d111551a5bcb70cbe8ee991c158b9ab7567654
raajsuriya roy choudhury <raajroyc@gmail.com>
2024-03-10 11:28:44 +0000

    Adding js event

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,6 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
+
+document.querySelector("html").addEventListener("click", function () {
+  alert("Ouch! Stop poking me!");
+});
