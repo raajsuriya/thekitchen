1e5a2fdc2986db00bf94a6b54833fa5cbdfe77b3
raajsuriya roy choudhury <raajroyc@gmail.com>
2024-03-10 19:52:09 +0000

    Dynamic table entry

--- scripts/main.js
+++ scripts/main.js
@@ -3,24 +3,46 @@ myHeading.textContent = "Inventory";
 
 let vegetables=["Broccoli","Cabbage","Tomato","Celery","Sugar Snaps","Chilli","Coriander Leaves",
 				"Cucumber","Rocket Leaves","Spinach","Kale","Potato","Turnips","Raddish",
-				"Carrot","Edemame","Zucchini"]
+				"Carrot","Edemame","Zucchini","Peas","Sweet Potato"]
 let fruits=["Figs","Lemon","Apple","Pears","Banana","Kiwi","Grapes","Passion Fruit","Orange",
 			"Grapefruit","Watermelon","Papaya","Strawberry","Blueberry","Raspberry","Blackberry",
-			"Plums"]
+			"Plums","Tamarind"]
 let botanicals=["Coriander Seeds","Fenugreek Seeds","Cloves","Cumin Seeds","Mustard Seeds",
-				"Cardomons","Sesame Seeds","Cinnamon","Fennel Seeds","Nigella Seeds"]
-let grains=["Rice","Yellow Lentil","Pinhead Oats","Rolled Oats","Oat Flakes"]
+				"Cardomons","Sesame Seeds","Cinnamon","Fennel Seeds","Nigella Seeds","Tumeric",
+				"Chia Seeds","Black Pepper","Salt"]
+let grains=["Rice","Yellow Lentil","Pinhead Oats","Rolled Oats","Oat Flakes","Red Kidney Beans",
+			"Black Beans","Green Gram"]
 
 
 let table_head=["Vegetables","#","£/u","Fruits","#","£/u","Botanicals","#","£/u","Grains","#","£/u"]
 
-for(var i=0;i<7;i++){
-	botanicals.push("")
-}
-for(var i=0;i<12;i++){
-	grains.push("")
+// for(var i=0;i<7;i++){
+// 	botanicals.push("")
+// }
+// for(var i=0;i<12;i++){
+// 	grains.push("")
+// }
+
+function equalize(){
+	let all_type_food=[vegetables,fruits,botanicals,grains];
+	var max=vegetables.length;
+	for(let foodtype of all_type_food){
+		// max < foodtype.length ? max=foodtype.length : max;
+		if(max<foodtype.length){
+			max=foodtype.length;
+		}
+	}
+	for(let foodtype of all_type_food){
+		if(foodtype.length<max){
+			var diff = max - foodtype.length
+			for(var k=0;k<diff;k++){
+				foodtype.push("");
+			}
+		}
+	}
 }
 
+equalize()
 var table='';
 table+='<tr>';
 for(let head of table_head){
